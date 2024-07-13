3996944825a9de9b6263dc6bdf7dde01dfd9beaf
Shomurod <sirojiddinshoakbarov0@gmail.com>
2024-07-14 00:19:15 +0500

    qo'shildi

--- src/routes/auth/register/Register.jsx
+++ src/routes/auth/register/Register.jsx
@@ -1,18 +1,8 @@
 import "./Register.css";
 import Button from "../../../utils/button/Button";
-import { useEffect, useState, useContext } from "react";
-import { toast } from "react-toastify";
-import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 const Register = () => {
-    let { t } = useTranslation();
-
-    let [passwordErrors, setPasswordErrors] = useState({
-        uppercase: true,
-        lovercase: true,
-        length: true,
-    });
-
     let [username, setUsername] = useState("");
     let [email, setEmail] = useState("");
     let [password, setPassword] = useState("");
@@ -24,62 +14,37 @@ const Register = () => {
     let [city, setCity] = useState("");
     let [country, setCountry] = useState("");
 
-    useEffect(() => {
-        setPasswordErrors({
-            lovercase: !/[a-z]/.test(password),
-            uppercase: !/[A-Z]/.test(password),
-            length: password.length >= 8 ? false : true,
-        });
-    }, [password]);
-
     let handleRegister = async (e) => {
         e.preventDefault();
 
         try {
-            if (
-                Object.values(passwordErrors).every((x) => x === false) &&
-                Object.values(usernameErrors).every((x) => x === false)
-            ) {
+            let response = await fetch(
+                "https://backend-e-commerce-production.up.railway.app/api/v1/users/register",
                 {
-                    dispatch({ type: "LOADING", loading: true });
-
-                    let response = await fetch(
-                        "https://backend-e-commerce-production.up.railway.app/api/v1/users/register",
-                        {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify({
-                                name: username,
-                                email,
-                                password,
-                                phone,
-                                isAdmin,
-                                street,
-                                apartment,
-                                zip,
-                                city,
-                                country,
-                            }),
-                        }
-                    );
-
-                    console.log(response);
-
-                    document.location.href = "/login";
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        name: username,
+                        email,
+                        password,
+                        phone,
+                        isAdmin,
+                        street,
+                        apartment,
+                        zip,
+                        city,
+                        country,
+                    }),
                 }
-            } else {
-                throw new Error("Xatolik yuz berdi");
-            }
+            );
+
+            console.log(response);
+
+            document.location.href = "/login";
         } catch (err) {
-            if (err.response) {
-                toast.error(err.response.status);
-            } else {
-                toast.error(err.status);
-            }
-        } finally {
-            dispatch({ type: "LOADING", loading: false });
+            console.log(err);
         }
     };
 
@@ -87,11 +52,11 @@ const Register = () => {
         <div className="auth">
             <div className="form-container">
                 <div className="form-wrapper">
-                    <h2 className="auth-title">{t("Register")}</h2>
+                    <h2 className="auth-title">Register</h2>
                     <form className="auth-form" onSubmit={handleRegister}>
                         <input
                             type="text"
-                            placeholder={t("Username")}
+                            placeholder="Username"
                             required
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
@@ -99,21 +64,11 @@ const Register = () => {
                         <ul></ul>
                         <input
                             type="password"
-                            placeholder={t("Password")}
+                            placeholder="Password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
-                        <ul>
-                            {passwordErrors.uppercase && (
-                                <li>{t("At least one uppercase letter")}</li>
-                            )}
-                            {passwordErrors.lovercase && (
-                                <li>{t("At least one lovercase letter")}</li>
-                            )}
-                            {passwordErrors.length && (
-                                <li>{t("At least 8 characters")}</li>
-                            )}
-                        </ul>
+
                         <input
                             type="email"
                             placeholder="email@domain.com"
@@ -170,7 +125,7 @@ const Register = () => {
                             onChange={(e) => setCountry(e.target.value)}
                             required
                         />
-                        <Button type="submit">{t("register")}</Button>
+                        <Button type="submit">register</Button>
                     </form>
                 </div>
             </div>
